7af573eb652345b2219e1d7ddc98b44546417618
faysal-sardouk <sardouk29@gmail.com>
2023-02-02 13:59:21 +0200

    2nd pushh

--- src/pages/main/components/topcreator/IndexCreator.js
+++ src/pages/main/components/topcreator/IndexCreator.js
@@ -1,1 +1,34 @@
+import img1 from './creatorimg/creator1.svg'
+import img2 from './creatorimg/creator2.svg'
+import img3 from './creatorimg/creator3.svg'
+import photo1 from './creatorimg/photo1.svg'
+import photo2 from './creatorimg/photo2.svg'
+import photo3 from './creatorimg/photo3.svg'
 
+
+const Data = [{
+    id: 1,
+    imgCreator:img1,
+    bckgrndimg:photo1,
+    name:Mgo,
+    description:"very nice"
+},
+{
+    id: 2,
+    imgCreator:img2,
+    bckgrndimg:photo2,
+    name:Mgo,
+    description:"very nice"
+},
+{
+    id: 3,
+    imgCreator:img3,
+    bckgrndimg:photo3,
+    name:Mgo,
+    description:"very nice"
+},
+
+
+]
+
+export default Data;
--- src/pages/main/components/topcreator/Topcreator.jsx
+++ src/pages/main/components/topcreator/Topcreator.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Topcreator.css'
-
+import Data from './IndexCreator';
 function TopCreator() {
   return (
  
