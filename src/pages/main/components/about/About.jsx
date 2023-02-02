77b3430a98af4d9cb051fa9f92fdbebdb29f071d
mgoyeghiaian <mgoyeghiaian@gmail.com>
2023-02-02 23:40:02 +0200

    Added About Section

--- src/pages/main/components/about/About.jsx
+++ src/pages/main/components/about/About.jsx
@@ -1,31 +1,34 @@
-import React from 'react'
-import Somthing from '../../../../assets/AboutImage.png';
+import React, { useState } from 'react'
+import Aboutimg from '../../../../assets/About.png';
 import './About.css'
 
 function About() {
+  const [showmore, Setshow] = useState();
   return (
-
     <div className='about-container'>
+
       <div className='about-img'>
-        <img src={Somthing} />
+        <img src={Aboutimg} />
+      </div>
+      <div className='about-header'>
+        <h1>About Us</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       </div>
       <div className='about-text'>
-        <div>
-          <div className='about-header'>
-            <h1>About Us</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </div>
-          <h1>Get Popular NFT</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinarLorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar</p>
-        </div>
-        <div className='about-button'>
-          <button className='button'>
-            Show More
-          </button>
-        </div>
+        <h1>Get Popular NFT</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinarLorem ipsum
+          <div className={`hiddenContent ${showmore ? 'show' : ''}`}> dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar</div></p>
       </div>
+      <div className='about-button'>
+        <button className='button ' id='myButton1' value="Show More" onClick={() => Setshow(!showmore)}>
+          Show More
+        </button>
+      </div>
+
+    </div>
 
   )
 }
 
 export default About
+
