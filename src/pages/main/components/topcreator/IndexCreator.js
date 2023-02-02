53ad2b1a5dadc7b282a249483c9fcd6c7855c63f
faysal-sardouk <sardouk29@gmail.com>
2023-02-02 16:46:30 +0200

    static

--- src/pages/main/components/topcreator/IndexCreator.js
+++ src/pages/main/components/topcreator/IndexCreator.js
@@ -1,34 +1,32 @@
-import img1 from './creatorimg/creator1.svg'
-import img2 from './creatorimg/creator2.svg'
-import img3 from './creatorimg/creator3.svg'
-import photo1 from './creatorimg/photo1.svg'
-import photo2 from './creatorimg/photo2.svg'
-import photo3 from './creatorimg/photo3.svg'
+import img1 from "./creatorimg/creator1.svg";
+import img2 from "./creatorimg/creator2.svg";
+import img3 from "./creatorimg/creator3.svg";
+import photo1 from "./creatorimg/photo1.svg";
+import photo2 from "./creatorimg/photo2.svg";
+import photo3 from "./creatorimg/photo3.svg";
 
-
-const Data = [{
+const Data = [
+  {
     id: 1,
-    imgCreator:img1,
-    bckgrndimg:photo1,
-    name:Mgo,
-    description:"very nice"
-},
-{
+    creatorimgr: img1,
+    bckgrndimg: photo1,
+    name: "Mgo",
+    descrip: "very nice",
+  },
+  {
     id: 2,
-    imgCreator:img2,
-    bckgrndimg:photo2,
-    name:Mgo,
-    description:"very nice"
-},
-{
+    creatorimg: img2,
+    bckgrndimg: photo2,
+    name: "Mgo",
+    descrip: "very nice",
+  },
+  {
     id: 3,
-    imgCreator:img3,
-    bckgrndimg:photo3,
-    name:Mgo,
-    description:"very nice"
-},
-
-
-]
+    creatorimg: img3,
+    bckgrndimg: photo3,
+    name: "Mgo",
+    descrip: "very nice",
+  },
+];
 
 export default Data;
--- src/pages/main/components/topcreator/Topcreator.jsx
+++ src/pages/main/components/topcreator/Topcreator.jsx
@@ -1,40 +1,34 @@
-import React from 'react'
-import './Topcreator.css'
-import Data from './IndexCreator';
+import React from "react";
+import "./Topcreator.css";
+import Data from "./IndexCreator";
 function TopCreator() {
   return (
- 
     <>
-    <div className="Main-creator">
-      <div className="Top-creator">
-        <h2 className="center">Top Creator</h2>
-        <p className="center"> Lorem5 asasasasasasdadadadadad </p>
-        <p className="center"> loremaaaaa</p>
-      </div>
-      <div className="Cards">
-        <div className="First-cards">
-          <img src="src/assets/icons/discordicon.svg" alt="nft" />
-         <p>card 1</p>
-         
+      <div className="Main-creator">
+        <div className="Top-creator">
+          <div className="nfts">
+            {Data.map((item, index) => {
+              return (
+                <div className="pics" key={index}>
+                  <img className="nft-img" src={item.bckgrndimg} />
 
+                  <div className="creator">
+                    <img className="firstpart-creator" src={item.creatorimg} />
+                    <p className="secondpart-creator">{item.name}</p>
+                    <p className="secondpart-creator">{item.descrip}</p>
+                  </div>
 
-
-
-
-
-
-
-
-
-        </div>
-        <div className="Second-cards">card 2
-        </div>
-        <div className="Third-cards">card 3
+                  <button className=" button collection-button ">
+                    +Follow
+                  </button>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
-  </>
-  )
+    </>
+  );
 }
 
 export default TopCreator;
