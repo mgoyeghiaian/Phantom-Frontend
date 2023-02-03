30618b3e93295f0eea728e001edb8e66d544cc59
Jihan Shamas <jihan.shamas@gmail.com>
2023-02-03 14:17:26 +0200

    1st step

--- src/pages/main/components/FAQ/Faq.jsx
+++ src/pages/main/components/FAQ/Faq.jsx
@@ -1,8 +1,51 @@
 import React from "react";
 import "./Faq.css";
-
 function Faq() {
-
+  const Faquestions = [
+    {
+      id: 1,
+      question: "How do i get paid?",
+      Answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      id: 2,
+      question:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit?",
+      Answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      id: 3,
+      question:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit?",
+      Answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      id: 4,
+      question:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit?",
+      Answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      id: 5,
+      question:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit?",
+      Answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      id: 6,
+      question:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit?",
+      Answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+  ];
 }
+// function Faq() {
+//   return (
+//     <div className="faq-container">
+//       <h1>Frequently Asked Questions</h1>
+//       <p>Wanna Ask Something?</p>
+//     </div>
+//   );
+// }
 
 export default Faq;
