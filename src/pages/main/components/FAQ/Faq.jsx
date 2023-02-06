a3c79a89e611e2dc3ffbe769a77f5d84fa4671e9
Jihan Shamas <jihan.shamas@gmail.com>
2023-02-06 14:14:20 +0200

    updated collection & faq

--- src/pages/main/components/FAQ/Faq.jsx
+++ src/pages/main/components/FAQ/Faq.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import "./Faq.css";
 import faquestions from "./faq";
+import faquestionsTwo from "./faq2";
+
 function Faq() {
   const [openAnswerIndex, setOpenAnswerIndex] = useState(-1);
+  const [rightOpenAnswerIndex, setRightOpenAnswerIndex] = useState(-1);
   return (
     <div className="faq-container">
       <div className="faq-title">
@@ -27,6 +30,34 @@ function Faq() {
                 </button>
               </div>
               {openAnswerIndex === index && (
+                <div className="faq-answer">
+                  <p>{item.answer}</p>
+                </div>
+              )}
+              <hr />
+            </div>
+          );
+        })}
+      </div>
+      {/* added another map */}
+      <div className="faq-qa">
+        {faquestionsTwo.map((item, index) => {
+          return (
+            <div className="faq-item" key={index}>
+              <div className="faq-qb">
+                <p className="faq-question">{item.question}</p>
+                <button
+                  className="faq-button"
+                  onClick={() =>
+                    setRightOpenAnswerIndex(
+                      rightOpenAnswerIndex === index ? -1 : index
+                    )
+                  }
+                >
+                  {rightOpenAnswerIndex === index ? "-" : "+"}
+                </button>
+              </div>
+              {rightOpenAnswerIndex === index && (
                 <div className="faq-answer">
                   <p>{item.answer}</p>
                 </div>
