b3105a7fef85ae2c0835f3d77a653b53a22d56ad
faysal-sardouk <sardouk29@gmail.com>
2023-02-07 22:11:10 +0200

    error in fetching data

--- src/pages/main/components/topcreator/Topcreator.jsx
+++ src/pages/main/components/topcreator/Topcreator.jsx
@@ -1,82 +1,79 @@
 import React from "react";
 import "./Topcreator.css";
 // import Data from "./IndexCreator";
+import axios from "axios";
+
 import { useState, useEffect } from "react";
 
 function TopCreator() {
-  // const [creator, setCreator] = useState([]);
-
-  // useEffect(() => {
-  //   fetchingCreators();
-  // }, []);
-
-  // const fetchingCreators = async () => {
-  //   const response = await fetch("/creator");
-  //   const data = response.json();
-  //   console.log(data);
-  //   setCreator(data);
-  //   console.log("Hello");
-  // };
-
-  const [creator, setCreators] = useState([]);
+  const [creator, setCreator] = useState([]);
 
   useEffect(() => {
-    // setIsLoading(true);
-    fetchCreators();
+    fetchingCreators();
   }, []);
 
-  const fetchCreators = async () => {
-    try {
-      const res = await fetch("http://localhost:8000/creator", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const resJson = await res.json();
-      setCreators(resJson.data);
-      console.log(creator);
-      // setIsLoading(false);
-    } catch (error) {
-      console.log(error);
-    }
+  const fetchingCreators = async () => {
+    const response = await fetch("/creator");
+    const data = response.json();
+    console.log(data);
+    setCreator(data);
+    console.log("Hello");
   };
+
+  // const [creator, setCreators] = useState([]);
+
+  // useEffect(() => {
+  //   // setIsLoading(true);
+  //   fetchCreators();
+  // }, []);
+
+  // const fetchCreators = async () => {
+  //   try {
+  //     const res = await fetch("/creator", {
+  //       method: "GET",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     });
+  //     const resJson = await res.json();
+  //     setCreators(resJson.data);
+  //     console.log("The creator", creator);
+  //   } catch (error) {
+  //     console.log("This is an error", error);
+  //   }
+  // };
 
   return (
     <>
       <div className="Main-creator">
         <div className="Top-creator">
           <div className="nfts">
-            {creator.map((item, index) => {
+            {creator.map((item) => {
               return (
                 // Section One
-                <div>Hello</div>
-                // <div className="card-container-creator" key={index}>
-                //   <div className="card-section-one">
-                //     <div className="backgroundcreator">
-                //       <img
-                //         className="nft-img"
-                //         src={item.bckgrndimg.data}
-                //         alt=""
-                //       />
-                //     </div>
-                //     <div className="image-creator">
-                //       <img src={item.creatorimg.data} alt="" />
-                //     </div>
-                //   </div>
-                //   {/* Section Two */}
-                //   <div className="card-section-two">
-                //     <div className="Description-creator">
-                //       <div>{item.name}</div>
-                //       <div>{item.descrip}</div>
-                //       <div>
-                //         <button className="button creator-button">
-                //           +Follow
-                //         </button>
-                //       </div>
-                //     </div>
-                //   </div>
-                // </div>
+
+                <div className="card-container-creator" key={item._id}>
+                  <div className="card-section-one">
+                    <div className="backgroundcreator">
+                      <img className="nft-img" src={item.bckgrndimg} alt="" />
+                    </div>
+                    <div className="image-creator">
+                      <img src={item.creatorimg} alt="" />
+                    </div>
+                  </div>
+
+                  <div className="card-section-two">
+                    <div className="Description-creator">
+                      <div>{item.name}</div>
+                      <div>{item.descrip}</div>
+                      <div>
+                        <button className="button creator-button">
+                          +Follow
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               );
             })}
           </div>
