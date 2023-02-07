8921c9c887e72adb7a4bf272f262aad800354dc2
faysal-sardouk <sardouk29@gmail.com>
2023-02-07 13:29:03 +0200

    static

--- src/pages/main/components/topcreator/Topcreator.jsx
+++ src/pages/main/components/topcreator/Topcreator.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 import "./Topcreator.css";
 import Data from "./IndexCreator";
+import { useState, useEffect } from "react";
+
 function TopCreator() {
+  // const [creator, setCreator] = useState([]);
+
+  // useEffect(() => {
+  //   fetchingCreators();
+  // }, []);
+
+  // const fetchingCreators = async () => {
+  //   const response = await fetch("/creator");
+  //   const data = response.json();
+  //   console.log(data);
+  //   setCreator(data);
+  //   console.log("Hello");
+  // };
+
   return (
     <>
       <div className="Main-creator">
@@ -9,24 +25,28 @@ function TopCreator() {
           <div className="nfts">
             {Data.map((item, index) => {
               return (
-                <div className="pics" key={index}>
-                  <div className="creator">
+                // Section One
+                <div className="card-container-creator" key={index}>
+                  <div className="card-section-one">
                     <div className="backgroundcreator">
                       <img className="nft-img" src={item.bckgrndimg} alt="" />
                     </div>
-                    <div className="image-creator"> 
-
-                    <img 
-                      src={item.creatorimg}
-                      alt=""/>
-                      </div>
-                    <p className= "centerp" >{item.name}</p>
-                    <p className= "centerp">{item.descrip}</p>
+                    <div className="image-creator">
+                      <img src={item.creatorimg} alt="" />
+                    </div>
                   </div>
-
-                  <button className=" button collection-button ">
-                    +Follow
-                  </button>
+                  {/* Section Two */}
+                  <div className="card-section-two">
+                    <div className="Description-creator">
+                      <div>{item.name}</div>
+                      <div>{item.descrip}</div>
+                      <div>
+                        <button className="button creator-button">
+                          +Follow
+                        </button>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               );
             })}
