4be5a31d717d3f4ef2a6b7e5eb145f03a6319ecf
mgoyeghiaian <mgoyeghiaian@gmail.com>
2023-02-08 08:02:16 +0200

    Added Loader

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Main from './pages/main/Main'
 import Login from './pages/Login/Login'
 import Privetrout from './pages/Login/Privetrout';
 import Dashboard from './pages/dashboard/Dashboard'
-
+import RingLoader from 'react-spinners/RingLoader'
+import Logo from '../src/assets/websiteicon/PHANTOM (2).svg'
 
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Main />} exact />
-        <Route path='/login' element={<Login />} />
-        <Route path="/dashboard" element={
-          <Privetrout>
-            <Dashboard />
-          </Privetrout>
-        } />
-      </Routes>
-    </BrowserRouter>
 
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  return (
+    <>
+      {
+        loading ?
+          <div className='loader'>
+            <img src={Logo} />
+            <RingLoader
+              color="#9228d3"
+              cssOverride={null}
+              loading
+              margin={15}
+              size={55}
+              speedMultiplier={1.1}
+            /> </div> :
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<Main />} exact />
+              <Route path='/login' element={<Login />} />
+              <Route path="/dashboard" element={
+                <Privetrout>
+                  <Dashboard />
+                </Privetrout>
+              } />
+            </Routes>
+          </BrowserRouter>
+      }
+    </>
   );
 
 }
