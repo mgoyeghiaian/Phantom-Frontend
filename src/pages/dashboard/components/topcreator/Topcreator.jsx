7fd3ef3fa0a7608e4d3c24c85ccf6029cb6948bc
faysal-sardouk <sardouk29@gmail.com>
2023-02-09 17:33:22 +0200

    ////

--- src/pages/dashboard/components/topcreator/Topcreator.jsx
+++ src/pages/dashboard/components/topcreator/Topcreator.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import './Topcreator.css'
+import './Topcreator.css'
 // import Data from './static/indexx'
 import { useState } from "react";
 import axios from "axios";
