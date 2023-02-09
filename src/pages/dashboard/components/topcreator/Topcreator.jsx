aa776f33c240331f647a17f2f0fd8c6f51c72b7d
faysal-sardouk <sardouk29@gmail.com>
2023-02-09 17:23:56 +0200

    working on dashboard

--- src/pages/dashboard/components/topcreator/Topcreator.jsx
+++ src/pages/dashboard/components/topcreator/Topcreator.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import './Topcreator.css'
+import React from "react";
+// import './Topcreator.css'
 // import Data from './static/indexx'
-import { useState } from 'react'
-import axios from 'axios'
-import { useEffect } from 'react'
+import { useState } from "react";
+import axios from "axios";
+import { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 
-function Creator(){const [creator, setCreator] = useState([]);
+function Creator() {
+  const [creator, setCreator] = useState([]);
 
   useEffect(() => {
     fetchingCreators();
@@ -14,41 +15,48 @@ function Creator(){const [creator, setCreator] = useState([]);
 
   const fetchingCreators = async () => {
     const response = await axios.get("http://localhost:3030/creators/");
-  
+
     console.log(response.data);
     setCreator(response.data);
     console.log("Hello");
   };
 
-
   return (
     <>
-      <div className="Main-creator">
-        <div className="Top-creator">
-          <div className="nfts">
-            {creator.map((item,index) => {
+      <div className="Main-creator-dashboard">
+        <div className="Top-creator-dashboard">
+          <div className="nfts--creator-dashboard">
+            {creator.map((item, index) => {
               return (
                 // Section One
+                // Add creator
+                //items
+                // creator name
+                // background image
+                // creator image
+                // description
 
-                <div className="card-container-creator" key={index}>
-                  <div className="card-section-one">
-                    <div className="backgroundcreator">
+                <div className="card-container-creator-dashboard" key={index}>
+                  <div className="card-section-one-dashboard">
+                    <div className="backgroundcreator-dashboard">
                       <img className="nft-img" src={item.bckgrndimg} alt="" />
                     </div>
-                    <div className="image-creator">
+                    <div className="image-creator-dashboard">
                       <img src={item.creatorimg} alt="" />
                     </div>
                   </div>
+                  {/* Section two
+                update creator */}
 
-                  <div className="card-section-two">
-                    <div className="Description-creator">
+                  <div className="card-section-two-dashboard">
+                    <div className="Description-creator-dashboard">
                       <div>{item.creatorName}</div>
                       <div>{item.description}</div>
                       <div>
-                        <button className="button creator-button">
+                        <button className="button creator-button-dashboard">
                           Update
                         </button>
-                        <button className="button creator-button">
+                        <button className="button creator-button-dashboard">
                           Delete
                         </button>
                       </div>
@@ -61,57 +69,7 @@ function Creator(){const [creator, setCreator] = useState([]);
         </div>
       </div>
     </>
+  );
+}
 
-
-
-
-
-
-
-
-
-
-    )}
-  
-  
-  
-  
-  
-  
-  
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- export default Creator;
+export default Creator;
