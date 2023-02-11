b960bf122239372161d43737b03210a7e8fa2e7e
mgoyeghiaian <mgoyeghiaian@gmail.com>
2023-02-11 13:32:13 +0200

    Try to navigate to home

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -56,7 +56,7 @@ function Login() {
       }
       );
       /** redirect to dashboard page */
-      window.location.replace('/');
+      window.location.replace('dashboard');
 
 
     } catch (error) {
